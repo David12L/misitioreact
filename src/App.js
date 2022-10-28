9d91df3d6624b8f2b297a016b1dbd194c545b1ef
luis2189p <luis2189p@gmail.com>
2022-10-27 19:28:06 -0500

    V02 my-app1 se modifico el nombre

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bienvendo david
         </a>
       </header>
     </div>
